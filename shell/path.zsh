bc7fbee355096cb3ed59179f053aba125f843597
mjsamuel <matthew.samuel@outlook.com.au>
2020-07-22 23:07:39 +1000

    Added pyenv to load path

--- shell/path.zsh
+++ shell/path.zsh
@@ -1,1 +1,7 @@
 export PATH=$PATH:/opt/apache-maven/bin
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="$HOME/.gem/ruby/2.7.0/bin:$PATH"
+
+export PATH="/Users/matthewsamuel/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
