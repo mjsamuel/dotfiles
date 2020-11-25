cd3f2439f4a5afe37039cb5fdd0222dbf171d837
mjsamuel <matthew.samuel@outlook.com.au>
2020-11-25 14:49:05 +1100

    Updated path

--- shell/path.zsh
+++ shell/path.zsh
@@ -2,7 +2,7 @@ export PATH=$PATH:/opt/apache-maven/bin
 export PATH="/usr/local/opt/ruby/bin:$PATH"
 export PATH="$HOME/.gem/ruby/2.7.0/bin:$PATH"
 
-export PATH="/Users/matthewsamuel/.pyenv/bin:$PATH"
+export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
@@ -10,3 +10,4 @@ export PATH="/usr/local/opt/tcl-tk/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/tcl-tk/lib"
 export CPPFLAGS="-I/usr/local/opt/tcl-tk/include"
 export PKG_CONFIG_PATH="/usr/local/opt/tcl-tk/lib/pkgconfig"
+export PATH="$PATH:$HOME/.rvm/bin"
