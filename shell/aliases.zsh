fcf4b54f0db173d576356c3ec911d5170af099f1
mjsamuel <matthew.samuel@outlook.com.au>
2020-12-02 16:06:40 +1100

    Updated aliases

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -2,3 +2,6 @@ alias ..='cd ..'
 alias c='clear'
 alias ls='exa'
 alias cat='bat'
+alias s='ssh'
+alias sshhosts="grep -w -i -E 'Host|HostName' ~/.ssh/config | sed 's/Host //' | sed 's/HostName //'"
+
