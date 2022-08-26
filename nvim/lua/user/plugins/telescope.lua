2f6470509c44b4b104c7c10d5318ad0cdc78a389
mjsamuel <matthew.samuel@outlook.com.au>
2022-08-26 19:32:09 +1000

    update telescope config

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -1,20 +1,43 @@
-local actions = require("telescope.actions")
-
 require("telescope").setup({
 	defaults = {
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
 		selection_caret = "  ",
 		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
 		sorting_strategy = "ascending",
 		layout_strategy = "horizontal",
-		mappings = {
-			i = {
-				["ESC"] = actions.close,
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
 			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
 		},
-		file_ignore_patterns = { ".git/" },
+		file_ignore_patterns = { ".git/", "node_modules" },
 		path_display = { "truncate" },
-		color_devicons = true,
+		winblend = 0,
 		border = {},
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+		color_devicons = true,
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 	},
 	pickers = {
 		find_files = {
