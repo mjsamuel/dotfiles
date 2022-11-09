6e755823c3fd6659a7e4ca32f845f8593a3fdc7b
mjsamuel <matthew.samuel@outlook.com.au>
2022-11-09 19:12:20 +1100

    update aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,11 +1,20 @@
+# misc
 alias c='clear'
 alias ls='exa'
 alias cat='bat'
-alias s='ssh'
-alias sshhosts="grep -w -i -E 'Host|HostName' ~/.ssh/config | sed 's/Host //' | sed 's/HostName //'"
 alias rtv='python3 -m rtv'
 alias tt='tt -theme ayu -showwpm'
 alias v='nvim'
 alias b='brew'
+
+# ssh
+alias s='ssh'
+alias sshhosts="grep -w -i -E 'Host|HostName' ~/.ssh/config | sed 's/Host //' | sed 's/HostName //'"
+
+# youtube-dl
 alias yt='yt-dlp --no-abort-on-error --embed-metadata -i'
-alias yta="yt -x -f bestaudio/best"
+alias yta="yt-dlp -x -f bestaudio/best"
+
+# tmux
+alias t="tmux"
+alias ta="tmux attach"
