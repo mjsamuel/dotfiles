5a8c03be90273ce85c8ec05dbeaac2dbc0e03a17
mjsamuel <matthew.samuel@outlook.com.au>
2023-01-03 20:35:29 +1100

    update nvim config
    - get rid of mini.nvim in favour of dedicated plugins
    - update dial config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 require("user.options")
 require("user.lazy")
 require("user.keybindings")
--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -11,7 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
-vim.g.mapleader = " "
 require("lazy").setup("user.plugins", {
 	defaults = { lazy = true },
 	install = {
