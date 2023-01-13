68ff00cd4b5d43d6a795d78e4a98405d45aba104
mjsamuel <matthew.samuel@outlook.com.au>
2023-01-13 14:04:32 +1100

    auto switch wezterrm theme to match system theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,20 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Gruvbox dark, medium (base16)"
+	else
+		return "Gruvbox Light"
+	end
+end
+
 return {
 	-- Appearance
 	font = wezterm.font("FiraCode Nerd Font Mono"),
 	font_size = 18,
-	color_scheme = "Gruvbox dark, medium (base16)",
-	window_padding = { left = 10, right = 1, top = 32, bottom = 0 },
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	window_padding = { left = 16, right = 16, top = 24, bottom = 0 },
 	-- Keybindings
 	keys = {
 		{ key = "Enter", mods = "ALT", action = act.DisableDefaultAssignment },
@@ -15,6 +23,6 @@ return {
 	-- Misc
 	window_close_confirmation = "NeverPrompt",
 	hide_tab_bar_if_only_one_tab = true,
-  window_decorations = "RESIZE",
-  native_macos_fullscreen_mode = true
+	window_decorations = "RESIZE",
+	native_macos_fullscreen_mode = true,
 }
