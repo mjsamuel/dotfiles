a6e101bbb420aa05253ac3c8b0d6133ecf016870
mjsamuel <matthew.samuel@outlook.com.au>
2023-03-10 18:41:30 +1100

    feat(nvim): add more ts injections

--- config/nvim/queries/typescript/injections.scm
+++ config/nvim/queries/typescript/injections.scm
@@ -1,3 +1,4 @@
+; Inject into '@Component({ template: `...` })' as html
 (decorator
   (call_expression
     function: (identifier) @function (#eq? @function "Component")
@@ -5,10 +6,16 @@
       (object
         (pair
           key: (property_identifier) @key (#eq? @key "template")
-          (template_string) @html
-        )
-      )
-    )
-  )
-)
+          ([ (template_string) @html (string) @html ]))))))
+
+; Inject into '@Component({ styles `...` })' as css
+(decorator
+  (call_expression
+    function: (identifier) @function (#eq? @function "Component")
+    (arguments
+      (object
+        (pair
+          key: (property_identifier) @key (#eq? @key "styles")
+          (array
+            ([ (template_string) @css (string) @css ])))))))
 
