1bb4c4df3f2e685a7b81468286fb6079ff5b3541
mjsamuel <matthew.samuel@outlook.com.au>
2023-03-19 22:50:17 +1100

    feat(wezterm): misc appearance changes

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -12,17 +12,18 @@ end
 return {
 	-- Appearance
 	font = wezterm.font("FiraCode Nerd Font Mono"),
-	font_size = 20,
+	font_size = 18,
 	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+  adjust_window_size_when_changing_font_size = false,
 	cursor_blink_rate = 0,
+	line_height = 1.1,
 	-- Keybindings
 	keys = {
 		{ key = "Enter", mods = "ALT", action = action.DisableDefaultAssignment },
 		{ key = "f", mods = "CMD", action = action.ToggleFullScreen },
 	},
 	-- Misc
-	window_close_confirmatio = "NeverPrompt",
 	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "RESIZE",
 	native_macos_fullscreen_mode = true,
