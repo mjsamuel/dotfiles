87c7cec513f08ac01fb332818a5d7f61bd7d7666
mjsamuel <matthew.samuel@outlook.com.au>
2024-01-13 14:55:38 +1100

    nvim: fix border highlights

--- config/nvim/lua/user/plugins/theme.lua
+++ config/nvim/lua/user/plugins/theme.lua
@@ -14,6 +14,7 @@ return {
     config = function()
       require("rose-pine").setup({
         disable_float_background = true,
+        extend_background_behind_borders = false
       })
       vim.cmd("colorscheme rose-pine")
     end,
