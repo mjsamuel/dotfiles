460d76c57aee7d00dffce7a5d47eb24a081beae6
mjsamuel <matthew.samuel@outlook.com.au>
2024-02-29 09:23:58 +1100

    nvim: reintroduce statuscol.nvim

--- config/nvim/lua/user/plugins/init.lua
+++ config/nvim/lua/user/plugins/init.lua
@@ -26,4 +26,24 @@ return {
     opts = {},
   },
   { "prichrd/netrw.nvim", lazy = false, opts = {} },
+  {
+    "luukvbaal/statuscol.nvim",
+    branch = "0.10",
+    event = "BufReadPost",
+    config = function()
+      vim.opt.numberwidth = 8
+      local builtin = require("statuscol.builtin")
+      require("statuscol").setup({
+        setopt = true,
+        ft_ignore = { "lazy", "harpoon" },
+        bt_ignore = { "nofile", "prompt", "quickfix", "help" },
+        segments = {
+          { sign = { namespace = { "diagnostic*", "Marks*" }, auto = false, maxwidth = 1, colwidth = 1 } },
+          { text = { " ", builtin.lnumfunc, " " } },
+          { sign = { namespace = { "gitsigns" }, auto = false, maxwidth = 1, colwidth = 1 } },
+          { text = { " " } },
+        },
+      })
+    end,
+  },
 }
