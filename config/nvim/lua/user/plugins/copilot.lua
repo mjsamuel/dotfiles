b925db9b56614240f7f84b87c375177d1ddba7bb
mjsamuel <matthew.samuel@outlook.com.au>
2024-12-03 08:31:54 +1100

    nvim: add Copilot chat plugin

--- config/nvim/lua/user/plugins/copilot.lua
+++ config/nvim/lua/user/plugins/copilot.lua
@@ -1,16 +1,60 @@
-local M = {
+local M = {}
+
+local suggestions = {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   event = "InsertEnter",
   cond = function() return vim.fn.executable("node") == 1 end,
+  opts = {
+    panel = { enabled = false },
+    suggestion = {
+      auto_trigger = true,
+      keymap = { accept = false },
+    },
+  }
 }
+table.insert(M, suggestions)
 
-M.opts = {
-  panel = { enabled = false },
-  suggestion = {
-    auto_trigger = true,
-    keymap = { accept = false },
-  },
+local chat = {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  build = "make tiktoken",
+  cmd = "CopilotChat",
+  config = function()
+    require("CopilotChat").setup({
+      prompts = {
+        -- Code related prompts
+        Explain = "Please explain how the following code works.",
+        Review = "Please review the following code and provide suggestions for improvement.",
+        Tests = "Please explain how the selected code works, then generate unit tests for it.",
+        Refactor = "Please refactor the following code to improve its clarity and readability.",
+        FixCode = "Please fix the following code to make it work as intended.",
+        FixError = "Please explain the error in the following text and provide a solution.",
+        BetterNamings = "Please provide better names for the following variables and functions.",
+        Documentation = "Please provide documentation for the following code.",
+        -- Text related prompts
+        Summarize = "Please summarize the following text.",
+        Spelling = "Please correct any grammar and spelling errors in the following text.",
+        Wording = "Please improve the grammar and wording of the following text.",
+        Concise = "Please rewrite the following text to make it more concise.",
+      },
+      question_header = " ",
+      answer_header = " ",
+      error_header = " ",
+      show_folds = false,
+      auto_follow_cursor = true,
+      auto_insert_mode = true
+    })
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      pattern = 'copilot-*',
+      callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+      end
+    })
+  end
 }
+table.insert(M, chat)
 
 return M
