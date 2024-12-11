5ee5a74dd9bb7f3052897c35b128990e8d272e1f
mjsamuel <matthew.samuel@outlook.com.au>
2024-12-12 10:08:19 +1100

    nvim: cleanup keybindings
    - remove lsp keybindings in favour of nvim defaults
    - update deprecated gitsign keybindings

--- config/nvim/lua/user/keybindings.lua
+++ config/nvim/lua/user/keybindings.lua
@@ -16,8 +16,8 @@ keymap.set("v", "sr", function() -- [s]earch using [r]ipgrep with visual selecti
 end)
 
 -- git
-keymap.set("n", "[g", function() require("gitsigns").prev_hunk() end, { silent = true })
-keymap.set("n", "]g", function() require("gitsigns").next_hunk() end, { silent = true })
+keymap.set("n", "[g", function() require("gitsigns").nav_hunk('prev') end, { silent = true })
+keymap.set("n", "]g", function() require("gitsigns").nav_hunk('next') end, { silent = true })
 keymap.set("v", "gr", function() require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end) -- [g]it [r]eset
 keymap.set("v", "gs", function() require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end) -- [g]it [s]tage
 keymap.set("n", "<C-g>", function() require("gitsigns").blame_line({ ignore_whitespace = true, full = true }) end)
@@ -28,17 +28,14 @@ vim.api.nvim_create_user_command("Diff", function()
 end, { nargs = 0 })
 
 -- lsp
-keymap.set("n", "D", function() vim.diagnostic.open_float() end)                         -- [D]iagnostics
-keymap.set("n", "gD", function() vim.lsp.buf.declaration() end)                          -- [g]o to [D]eclaration
-keymap.set("n", "gd", function() vim.lsp.buf.definition() end)                           -- [g]o to [d]efinition
-keymap.set("n", "gi", function() require("telescope.builtin").lsp_implementations() end) -- [g]o to [i]mplementation
-keymap.set("n", "gr", function() require("telescope.builtin").lsp_references() end)      -- [g]o to [r]eferences
-keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end)                      -- [g]o to [t]ype definition
-keymap.set({ "n", "v" }, "<Leader>f", function() vim.lsp.buf.format() end)               -- [f]ormat
-
--- code [r]efactoring/fixing
-keymap.set("n", "<Leader>rr", function() vim.lsp.buf.rename() end) -- [r]ename
-keymap.set({ "n", "v" }, "<Leader>ra", function()                  -- [a]ction
+keymap.set("n", "D", function() vim.diagnostic.open_float() end)           -- [D]iagnostics
+keymap.set("n", "gd", function() vim.lsp.buf.definition() end)             -- [g]o to [d]efinition
+keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end)        -- [g]o to [t]ype definition
+keymap.set({ "n", "v" }, "<Leader>f", function() vim.lsp.buf.format() end) -- [f]ormat
+-- overriding default behaviour with telescope picker
+keymap.set("n", "gri", function() require("telescope.builtin").lsp_implementations() end)
+keymap.set("n", "grr", function() require("telescope.builtin").lsp_references() end)
+keymap.set({ "n", "v" }, "gra", function() -- [a]ction
   -- in case telescope has not lazily loaded
   require("telescope")
   vim.lsp.buf.code_action()
@@ -62,27 +59,24 @@ for i, key in ipairs({ "w", "f", "p", "r", "s", "t", "x", "c", "v" }) do       -
   keymap.set("n", "m" .. i, function() require("harpoon.ui").nav_file(i) end)
 end
 
--- yank
-keymap.set("n", "<leader>y", '<cmd>let @+=@" | echo "Copied to system clipboard"<cr>', { silent = true })
-
 -- make
 local function make(make_args)
   if not vim.g.makefile_exists then return end
   vim.cmd("wa | silent make " .. (make_args or ""))
-  if #vim.fn.getqflist() > 0 then
-    vim.cmd("copen")
-  else
+  if #vim.fn.getqflist() == 0 then
     vim.cmd("cclose")
+    return
   end
+  vim.cmd("copen")
 end
 keymap.set("n", "gm", function() make() end, { silent = true })
 
 -- copilot
-local RIGHT_KEY = vim.api.nvim_replace_termcodes("<Right>", true, false, true)
 keymap.set("i", "<Right>", function()
   local cp = require("copilot.suggestion")
+  local right_key = vim.api.nvim_replace_termcodes("<Right>", true, false, true)
   if not cp.is_visible() then
-    vim.api.nvim_feedkeys(RIGHT_KEY, "n", false)
+    vim.api.nvim_feedkeys(right_key, "n", false)
     return
   end
   cp.accept()
@@ -97,3 +91,4 @@ vim.api.nvim_create_user_command("Wqa", function() vim.cmd(":wqa") end, { nargs 
 
 -- misc
 keymap.set("n", "\\", function() require("oil").open() end, { silent = true })
+keymap.set("n", "<leader>y", '<cmd>let @+=@" | echo "Copied to system clipboard"<cr>', { silent = true })
