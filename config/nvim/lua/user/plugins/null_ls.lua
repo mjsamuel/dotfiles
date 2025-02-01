d37390f4bc2e72995f9c92a5ca869cf4597d49be
mjsamuel <hello@mjsamuel.com>
2025-02-01 13:02:54 +1100

    nvim: fix prettier formatting not applying to go-templates

--- config/nvim/lua/user/plugins/null_ls.lua
+++ config/nvim/lua/user/plugins/null_ls.lua
@@ -4,8 +4,21 @@ local M = {
 }
 
 M.config = function()
-  require("null-ls").setup({})
-  require("mason-null-ls").setup({ handlers = {} })
+  local null_ls = require("null-ls")
+  local mason_null_ls = require("mason-null-ls")
+
+  null_ls.setup({})
+  mason_null_ls.setup({
+    ensure_installed = {},
+    automatic_installation = false,
+    handlers = {
+      prettierd = function()
+        local config = null_ls.builtins.formatting.prettierd
+        config.filetypes = vim.tbl_extend("force", config.filetypes, { "gotmpl" })
+        null_ls.register(config)
+      end,
+    }
+  })
 end
 
 return M
