2fad6b640ce2a07a6ec69b6abf9ea49927a25142
mjsamuel <hello@mjsamuel.com>
2025-04-06 21:03:48 +1000

    nvim: sort diagnostics by severity

--- config/nvim/lua/user/plugins/lspconfig.lua
+++ config/nvim/lua/user/plugins/lspconfig.lua
@@ -13,23 +13,6 @@ function M.config()
     function(server_name) -- default handler
       lspconfig[server_name].setup({ capabilities = capabilities })
     end,
-    ["tailwindcss"] = function()
-      local filetypes = lspconfig["tailwindcss"].config_def.default_config.filetypes
-      table.insert(filetypes, "gotmpl")
-      lspconfig["tailwindcss"].setup({
-        capabilities = capabilities,
-        filetypes = filetypes,
-        root_dir = function(fname)
-          local root_pattern = lspconfig.util.root_pattern(
-            "tailwind.config.js",
-            "tailwind.config.cjs",
-            "tailwind.config.mjs",
-            "tailwind.config.ts"
-          )
-          return root_pattern(fname)
-        end,
-      })
-    end,
     ["emmet_ls"] = function()
       local filetypes = lspconfig["emmet_ls"].config_def.default_config.filetypes
       table.insert(filetypes, "gotmpl")
