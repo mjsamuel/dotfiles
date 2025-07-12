173b5ed546ab1b258e5f5c8c01af2d4c68e42aea
mjsamuel <hello@mjsamuel.com>
2025-07-12 22:16:48 +1000

    tmux: update app-icon map

--- config/tmux/app-icon.sh
+++ config/tmux/app-icon.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
-window_name="$1"
-pane_current_command="$2"
+# Usage: ./app-icon.sh <tmux_window_name>
 
-case "$window_name" in
-logs)
-    echo '#[fg=white]#[default]'
-    exit
-    ;;
-esac
-
-case "$pane_current_command" in
+case "$1" in
 *git*) echo '#[fg=red]#[default]' ;;
+ai | claude*) echo '#[fg=orange]󰫢#[default]' ;;
+air) echo '#[fg=blue]#[default]' ;;
 bat) echo '#[fg=blue]󰄛#[default]' ;;
 docker) echo '#[fg=blue]#[default]' ;;
 go) echo '#[fg=blue]#[default]' ;;
-air) echo '#[fg=blue]#[default]' ;;
 java) echo '#[fg=red]#[default]' ;;
 jira) echo '#[fg=blue]󰌃#[default]' ;;
+logs) echo '#[fg=white]#[default]' ;;
 man) echo '#[fg=blue]󰈙#[default]' ;;
 newsboat) echo '#[fg=orange]#[default]' ;;
 node | npm) echo '#[fg=green]󰆧#[default]' ;;
-nvim) echo '#[fg=green]#[default]' ;;
+nvim | code) echo '#[fg=green]#[default]' ;;
 python) echo '#[fg=yellow]#[default]' ;;
 ruby) echo '#[fg=red]#[default]' ;;
 tt) echo '#[fg=white]󰌌#[default]' ;;
