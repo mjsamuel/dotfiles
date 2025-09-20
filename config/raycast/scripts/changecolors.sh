f5167683dade5efb48431b47513233d176c9a184
mjsamuel <hello@mjsamuel.com>
2025-09-20 12:12:10 +1000

    raycast: include config

---
+++ config/raycast/scripts/changecolors.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Change Colors
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌒
+# @raycast.packageName change.colors.mjsamuel.com
+
+# Documentation:
+# @raycast.description Toggle the system theme
+# @raycast.author Matthew Samuel
+# @raycast.authorURL mjsamuel.com
+
+changecolors
