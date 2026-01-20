ea007f8d6d8377459b95b5c21a99818102c3f971
mjsamuel <hello@mjsamuel.com>
2026-01-20 15:40:36 +1100

    tmux: update app icon script

--- config/tmux/app-icon.sh
+++ config/tmux/app-icon.sh
@@ -1,23 +1,42 @@
 #!/bin/sh
-# Usage: ./app-icon.sh <tmux_window_name>
+# Script to return an app icon based on the tmux window name or current pane command.
+# Usage: ./app-icon.sh <tmux_window_name> <pane_current_command> <is_window_selected>
+window_name="$1"
+pane_current_command="$2"
+is_selected="$3"
 
-case "$1" in
-*git*) echo '#[fg=red]#[default]' ;;
-ai | claude*) echo '#[fg=orange]󰫢#[default]' ;;
-air) echo '#[fg=blue]#[default]' ;;
-bat) echo '#[fg=blue]󰄛#[default]' ;;
-docker) echo '#[fg=blue]#[default]' ;;
-go) echo '#[fg=blue]#[default]' ;;
-java) echo '#[fg=red]#[default]' ;;
-jira) echo '#[fg=blue]󰌃#[default]' ;;
-logs) echo '#[fg=white]#[default]' ;;
-man) echo '#[fg=blue]󰈙#[default]' ;;
-newsboat) echo '#[fg=orange]#[default]' ;;
-node | npm) echo '#[fg=green]󰆧#[default]' ;;
-nvim | code) echo '#[fg=green]#[default]' ;;
-python) echo '#[fg=yellow]#[default]' ;;
-ruby) echo '#[fg=red]#[default]' ;;
-tt) echo '#[fg=white]󰌌#[default]' ;;
-vim) echo '#[fg=green]#[default]' ;;
-*) echo '#[fg=orange]#[default]' ;;
+return_app() {
+	app_icon="$1"
+	color="$2"
+	if [ "$is_selected" -eq 0 ]; then
+		color="gray"
+	fi
+	printf "#[fg=%s]%s#[default]" "$color" "$app_icon"
+	exit 0
+}
+
+case "$window_name" in
+logs) return_app '' 'white' ;;
+ai | claude*) return_app '󰫢' 'orange' ;;
+esac
+
+case "$pane_current_command" in
+*git*) return_app "" "red" ;;
+air) return_app "" "blue" ;;
+bat) return_app "󰄛" "blue" ;;
+docker) return_app "" "blue" ;;
+go) return_app "" "blue" ;;
+java) return_app "" "red" ;;
+jira) return_app "󰌃" "blue" ;;
+man) return_app "󰈙" "blue" ;;
+newsboat) return_app "" "orange" ;;
+node | npm) return_app "󰆧" "green" ;;
+nvim) return_app "" "green" ;;
+python) return_app "" "yellow" ;;
+ruby) return_app "" "red" ;;
+ssh) return_app "󰌘" "white" ;;
+tt) return_app "󰌌" "white" ;;
+vim) return_app "" "green" ;;
+yt-dl*) return_app "󰇚" "green" ;;
+*) return_app "" "orange" ;;
 esac
