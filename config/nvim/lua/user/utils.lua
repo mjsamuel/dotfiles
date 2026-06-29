57637d27954252241c15c7477364cb94ce4d0d98
mjsamuel <hello@mjsamuel.com>
2026-06-29 19:29:11 +1000

    nvim: fix text selection util

--- config/nvim/lua/user/utils.lua
+++ config/nvim/lua/user/utils.lua
@@ -27,7 +27,7 @@ M.get_visual_selection = function()
 
   local start_pos = vim.api.nvim_buf_get_mark(0, "<")
   local end_pos = vim.api.nvim_buf_get_mark(0, ">")
-  local lines = vim.api.nvim_buf_get_text(0, start_pos[1] - 1, start_pos[2], end_pos[1] - 1, end_pos[2], {})
+  local lines = vim.api.nvim_buf_get_text(0, start_pos[1] - 1, start_pos[2], end_pos[1] - 1, end_pos[2] + 1, {})
   return {
     text = table.concat(lines, "\n"),
     pos = start_pos,
