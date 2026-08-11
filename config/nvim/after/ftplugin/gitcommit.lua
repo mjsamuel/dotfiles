348b16b73b6618346e5bc5f9d9a10a9b522a449f
mjsamuel <hello@mjsamuel.com>
2026-08-11 19:21:04 +1000

    nvim: tweak filetype specific config

--- config/nvim/after/ftplugin/gitcommit.lua
+++ config/nvim/after/ftplugin/gitcommit.lua
@@ -3,4 +3,4 @@ vim.opt_local.wrap = true
 
 local textwidth = 80
 vim.opt_local.textwidth = textwidth
-vim.opt_local.colorcolumn = tostring(textwidth)
+vim.opt_local.colorcolumn = tostring(textwidth + 1)
--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,8 @@
 vim.opt_local.spell = true
 vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+
+vim.keymap.set('n', 'j', 'gj', { buffer = true })
+vim.keymap.set('n', 'k', 'gk', { buffer = true })
+vim.keymap.set('n', '^', 'g^', { buffer = true })
+vim.keymap.set('n', '$', 'g$', { buffer = true })
